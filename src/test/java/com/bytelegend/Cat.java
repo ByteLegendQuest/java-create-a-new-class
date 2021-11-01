3257ff24e65f3bfba1e9e7172b2ad3295a069548
Ryan.Zhao <ryan.zhao@seedlinktech.com>
2021-11-01 17:01:52 +0800

    Add Cat

--- src/test/java/com/bytelegend/Cat.java
+++
@@ -1,7 +0,0 @@
-package com.bytelegend;
-
-public class Cat {
-    public static void main() {
-        System.out.println("I am a cute cat.");
-    }
-}
--- src/test/java/com/bytelegend/CatTest.java
+++ src/test/java/com/bytelegend/CatTest.java
@@ -3,6 +3,9 @@ package com.bytelegend;
 import org.junit.jupiter.api.Test;
 
 public class CatTest {
+    public class Cat {
+    }
+
     @Test
     public void testNewCat() {
         new Cat();
