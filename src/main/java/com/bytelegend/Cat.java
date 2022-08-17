4281f59eb17543e98ef4ce2cafd5bdf714c71313
Volta-wang <1213428717@qq.com>
2022-08-17 15:59:45 +0000

    Update at 2022-08-17T15:59:45.909558Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,2 +1,44 @@
-    publice class Cat
+public class Cat {
+
+private String name;
+
+public Cat(String name){
+
+this.name=name;
+
+}
+
+public String show(){
+
+return name;
+
+}
+
+}
+
+package com.zw.work;
+
+import java.util.Iterator;
+
+import java.util.LinkedList;
+
+import java.util.List;
+
+public class CatTest {
+
+public static void main(String[] args) {
+
+Listlist=new LinkedList();
+
+list.add(new Cat("白猫"));
+
+list.add(new Cat("黑猫"));
+
+list.add(new Cat("花猫"));
+
+for (Cat cat : list) {
+
+System.out.println(cat.show());
+
+}
 
