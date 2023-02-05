59aad03fcab9fac09d1d5a1ff87f50684b46d834
taohongsheng <842924773@qq.com>
2023-02-05 07:29:44 +0000

    Update at 2023-02-05T07:29:44.003951Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,18 +1,5 @@
-public class Dog {
-    String breed;
-    int size;
-    String colour;
-    int age;
- 
-    void eat() {
-    }
- 
-    void run() {
-    }
- 
-    void sleep(){
-    }
- 
-    void name(){
-    }
+public clsaa Cat{
+  public class main void(String[] args){
+    System.out.println("hello,world");
+  }
 }
