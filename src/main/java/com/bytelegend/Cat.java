899290a044faa9a50ba09ec49534acda9c0eed87
taohongsheng <842924773@qq.com>
2023-02-05 07:24:39 +0000

    Update at 2023-02-05T07:24:39.032516Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,3 +1,15 @@
-public class  void Cat{
-
-}
+ private String name;
+  private int age;
+  
+  public Student(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public int getAge() {
+    return age;
+  }
