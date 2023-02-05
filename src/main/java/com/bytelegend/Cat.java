dab7a1df929f118d231f7e7209d5cf4501f9e328
taohongsheng <842924773@qq.com>
2023-02-05 07:33:07 +0000

    Update at 2023-02-05T07:33:07.111897Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,18 +1,3 @@
 public class Cat {
-    String breed;
-    int size;
-    String colour;
-    int age;
- 
-    void eat() {
-    }
- 
-    void run() {
-    }
- 
-    void sleep(){
-    }
- 
-    void name(){
-    }
+
 }
