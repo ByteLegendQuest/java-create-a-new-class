17fd673d553f208be46186ac17f88b41fbe1b8ee
DeerSayHello <deerzfhu@126.com>
2023-03-23 10:22:15 +0000

    Update at 2023-03-23T10:22:15.604823Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,5 +1,6 @@
 public class Cat {
+    String cat = "Im a Cat!";
     public static void main (String[] args) {
-           System.out.println ("Im a cat!"); 
+           System.out.println (cat); 
     }
 }
