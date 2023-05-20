ad9fee128e672c7c89b6e9e12d0c1ad29c0e4802
hanglovebear <2863783182@qq.com>
2023-05-20 16:31:16 +0000

    Update at 2023-05-20T16:31:16.857591Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,11 +1,11 @@
-public class cat {
+public class Cat {
     private String name;
     private int age;
-    public void cat(String name, int age) {
+    public void Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
-    public void sayMiao() {
+    public void sayHello() {
         System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
     }
 }
