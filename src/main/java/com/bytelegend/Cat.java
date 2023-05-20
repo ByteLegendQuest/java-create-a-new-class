a9c9ef1510f52dca85310dd76c67e574aebe29cf
hanglovebear <2863783182@qq.com>
2023-05-20 16:42:42 +0000

    Update at 2023-05-20T16:42:42.133538Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,7 +1,7 @@
 public class Cat {
     private final String name;
     private int age;
-    public Cat(String name,int age) {
+    public Cat(String name, int age) {
         this.name = name;
         this.age = age;
     }
