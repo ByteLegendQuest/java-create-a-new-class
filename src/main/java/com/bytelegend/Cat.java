01be03ad6a62a6b07df45eea097e9be635ad7adb
hanglovebear <2863783182@qq.com>
2023-05-20 17:05:51 +0000

    Update at 2023-05-20T17:05:51.262958Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -1,3 +1,29 @@
 public class Cat {
-    // 类的属性和方法可以在这里定义
+    private String name;
+    private int age;
+    // 构造函数
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    // 获取名字
+    public String getName() {
+        return name;
+    }
+    // 设置名字
+    public void setName(String name) {
+        this.name = name;
+    }
+    // 获取年龄
+    public int getAge() {
+        return age;
+    }
+    // 设置年龄
+    public void setAge(int age) {
+        this.age = age;
+    }
+    // 自我介绍方法
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
 }
